af567866e58d992abcd8aea9323ebc18e5f04e9d
Reezan Visram <reezan.visram@rogers.com>
2024-08-20 22:37:52 -0400

    working commit

--- Testbenches/tb_i2s_controller_rx_tx.v
+++ Testbenches/tb_i2s_controller_rx_tx.v
@@ -1,17 +1,34 @@
 `timescale 1ns / 1ps
 
 module tb_i2s_controller_rx_tx;
-
+  parameter DATA_WIDTH=32;
   reg clk = 1'b0;
   reg resetn = 1'b0;
   wire mclk;
-  wire ws;
   wire sck;
+  wire ws;
 
   wire [31:0] data_left;
   wire [31:0] data_right;
+  reg sd_pre;
   reg sd;
   wire sdout;
+
+  event capture_output;
+  event check_output;
+
+  reg [DATA_WIDTH - 1:0] captured_sample;
+
+  reg [DATA_WIDTH - 1:0] prev_sent_left;
+  reg [DATA_WIDTH - 1:0] prev_sent_right;
+  reg [DATA_WIDTH - 1:0] prev_sent_leftd;
+  reg [DATA_WIDTH - 1:0] prev_sent_rightd;
+
+  reg [DATA_WIDTH - 1:0] sent;
+
+  integer i;
+  integer num_fails = 0;
+  integer num_tests = 4;
 
   i2s_controller master(.clk(clk), .resetn(resetn), .mclk(mclk), .ws(ws), .sck(sck));
 
@@ -37,15 +54,57 @@ module tb_i2s_controller_rx_tx;
   
   initial
   begin
-    random_sample = $random;
+    $timeformat(-9, 2, " ns", 20);
     #15 resetn = 1'b1;
-    repeat(32)
+    wait(resetn);
+    repeat(num_tests)
     begin
-      sd <= random_sample[31];
-      random_sample <= random_sample << 1;
-      @(negedge sck);
+      random_sample = $random;
+      sent = random_sample;
+      for (i = 0; i < DATA_WIDTH; i = i + 1)
+      begin
+        sd_pre <= random_sample[DATA_WIDTH - 1];
+        sd <= sd_pre;
+        random_sample <= random_sample << 1;
+        captured_sample[DATA_WIDTH - 1 - i] <= sdout;
+        @(negedge sck);
+      end
+      sd <= sd_pre;
+
+      if (ws)
+        prev_sent_left <= sent;
+      else
+        prev_sent_right <= sent;
+
+      ->check_output;
     end
+
+    $display("%0d/%0d tests passed!", num_tests - num_fails, num_tests);
+
     #20000 $finish;
+  end
+
+  always @(check_output)
+  begin
+    @(negedge sck);
+    prev_sent_leftd = prev_sent_left;
+    prev_sent_rightd = prev_sent_right; 
+    if (ws)
+    begin
+      if (captured_sample !== prev_sent_leftd)
+      begin
+        $display("Test Failed: Time = %0t, Expected = %0h, Received = %0h", $time, prev_sent_leftd, captured_sample);
+        num_fails = num_fails + 1;
+      end
+    end
+    else
+    begin
+      if (captured_sample !== prev_sent_rightd)
+      begin
+        $display("Test Failed: Time = %0t, Expected = %0h, Received = %0h", $time, prev_sent_rightd, captured_sample);
+        num_fails = num_fails + 1;
+      end
+    end
   end
 
   always
